cbd186f1aa39c21dd368d9ec7db6346c8bac6d73
Splicho <d.cacian@gmx.de>
2024-09-28 03:03:42 +0200

    debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: process.env.NODE_ENV === 'production'
+      ? process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://your-domain.com'
+      : 'http://localhost:3000',
+  },
+  // Enable static exports for ISR
+  output: 'standalone',
+};
 
 export default nextConfig;
